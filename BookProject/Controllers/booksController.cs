41f7cb6503ca96dab0f785711f03a480618b4acd
YusufGND <yusufgundogdu112@gmail.com>
2022-12-21 17:23:22 +0300

    first commit

---
+++ BookProject/Controllers/booksController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookProject.Data;
+using BookProject.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace BookProject.Controllers
+{
+    public class booksController : Controller
+    {
+        private readonly BookProjectContext _context;
+
+        public booksController(BookProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: books
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.book.ToListAsync());
+        }
+
+        public async Task<IActionResult> catalogue()
+        {
+            return View(await _context.book.ToListAsync());
+        }
+
+       
+
+        // GET: books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: books/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IFormFile file, [Bind("Id,title,info,bookquantity,price,cataid,author")] book book)
+        {
+            if (file != null)
+            {
+                string filename = file.FileName;
+                //  string  ext = Path.GetExtension(file.FileName);
+                string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images"));
+                using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                { await file.CopyToAsync(filestream); }
+
+                book.imgfile = filename;
+            }
+
+            //if (ModelState.IsValid)
+            //{
+            _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            //}
+            //return View(book);
+        }
+
+        // GET: books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(IFormFile file, int id, [Bind("Id,title,info,bookquantity,price,cataid,author,imgfile")] book book)
+        {
+
+            if (file != null)
+            {
+                string filename = file.FileName;
+                //  string  ext = Path.GetExtension(file.FileName);
+                string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images"));
+                using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+                { await file.CopyToAsync(filestream); }
+                book.imgfile = filename;
+            }
+            //if (id != book.Id)
+            //{
+            //    return NotFound();
+            //}
+
+            //if (ModelState.IsValid)
+            //{
+            //    try
+            //    {
+
+
+            _context.Update(book);
+                    await _context.SaveChangesAsync();
+                //}
+                //catch (DbUpdateConcurrencyException)
+                //{
+                //    if (!bookExists(book.Id))
+                //    {
+                //        return NotFound();
+                //    }
+                //    else
+                //    {
+                //        throw;
+                //    }
+                //}
+                return RedirectToAction(nameof(Index));
+            //}
+            //return View(book);
+        }
+
+        // GET: books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await _context.book.FindAsync(id);
+            _context.book.Remove(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool bookExists(int id)
+        {
+            return _context.book.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BookProject/Data/BookProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookProject.Models;
+
+namespace BookProject.Data
+{
+    public class BookProjectContext : DbContext
+    {
+        public BookProjectContext (DbContextOptions<BookProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookProject.Models.book> book { get; set; }
+
+        public DbSet<BookProject.Models.usersaccounts> usersaccounts { get; set; }
+
+        public DbSet<BookProject.Models.orders> orders { get; set; }
+        public DbSet<BookProject.Models.report> report { get; set; }
+
+    }
+}
